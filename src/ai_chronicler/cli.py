30b6d4989112045d73307a82328d3179384de969
b-thomson <benalexanderthomson@gmail.com>
2025-09-21 23:49:25 +0200

    feat(extract): add argparse CLI

--- src/ai_chronicler/cli.py
+++ src/ai_chronicler/cli.py
@@ -1,8 +1,15 @@
-import sys
+import argparse
 from extract import extract
 
+
 def main():
-    extract(sys.argv[1],sys.argv[2])
+    parser = argparse.ArgumentParser(description="Extract conversations.json from zipped ChatGPT exports")
+    parser.add_argument("src_dir", help = "Source directory for the data export .zip files")
+    parser.add_argument("out_dir", help = "Target directory to write the extracted conversations.json files")
+    args = parser.parse_args()
+
+    print(f"Extracting from {args.src_dir} → {args.out_dir}")
+    extract(src_dir=args.src_dir,out_dir=args.out_dir)
 
 if __name__ == "__main__":
     main()
--- src/ai_chronicler/extract.py
+++ src/ai_chronicler/extract.py
@@ -5,17 +5,17 @@ import zipfile
 file_ext = ".zip"
 conv_file = "conversations.json"
 
-def extract(zip_dir: Path, output_dir: Path):
+def extract(src_dir: Path, out_dir: Path):
     
-    for zip_file in os.listdir(zip_dir):
+    for zip_file in os.listdir(src_dir):
 
         if zip_file.endswith(file_ext):
             zip_file_stem = Path(zip_file).stem
-            source_file_path = os.path.join(zip_dir,zip_file)
+            source_file_path = os.path.join(src_dir,zip_file)
 
             with zipfile.ZipFile(source_file_path) as zf:
-                zf.extract(conv_file, output_dir)
+                zf.extract(conv_file, out_dir)
             
-            rename_source = os.path.join(output_dir,conv_file)
-            rename_target = os.path.join(output_dir,f"{zip_file_stem}_{conv_file}")
+            rename_source = os.path.join(out_dir,conv_file)
+            rename_target = os.path.join(out_dir,f"{zip_file_stem}_{conv_file}")
             os.rename(rename_source,rename_target)
